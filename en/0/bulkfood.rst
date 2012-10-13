a97bca7ca7029ff71c82d60144fa830733c03bc7
Luciano Ramalho <luciano@ramalho.org>
2012-10-13 00:10:11 -0300

    new step one with regular getter/setter

--- en/0/bulkfood.rst
+++ en/0/bulkfood.rst
@@ -6,14 +6,7 @@ A line item for a bulk food order has description, weight and price fields::
 
 	>>> from bulkfood import LineItem
 	>>> raisins = LineItem('Golden raisins', 5, 2.48)
-	>>> raisins.description, raisins.get_weight(), raisins.price
+	>>> raisins.description, raisins.weight, raisins.price
 	('Golden raisins', 5, 2.48)
 	>>> raisins.subtotal()
 	12.4
-
-The weight of a ``LineItem`` must be > 0::
-
-	>>> raisins.set_weight(-10)
-	Traceback (most recent call last):
-		...
-	ValueError: value must be > 0
--- en/1/bulkfood.py
+++ en/1/bulkfood.py
@@ -2,8 +2,17 @@ class LineItem(object):
 
     def __init__(self, description, weight, price):
         self.description = description
-        self.weight = weight
+        self.set_weight(weight)
         self.price = price
-        
+
     def subtotal(self):
-        return self.weight * self.price
+        return self.get_weight() * self.price
+
+    def get_weight(self):
+        return self.__weight
+
+    def set_weight(self, value):
+        if value > 0:
+            self.__weight = value
+        else:
+            raise ValueError('value must be > 0')
