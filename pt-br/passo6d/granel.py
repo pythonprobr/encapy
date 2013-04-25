6f91d32e4265867f6919f072be3841d57463bb3b
Luciano Ramalho <luciano@ramalho.org>
2013-04-25 13:03:40 -0300

    retirei definicao redundante do descriptor Quantidade

--- pt-br/passo6d/granel.py
+++ pt-br/passo6d/granel.py
@@ -1,22 +1,5 @@
 from modelo import Quantidade, entidade
 
-class Quantidade(object):
-
-    def __init__(self):
-        self.set_nome(self.__class__.__name__, id(self))
-
-    def set_nome(self, prefix, key):
-        self.nome_alvo = '%s_%s' % (prefix, key)
-
-    def __get__(self, instance, owner):
-        return getattr(instance, self.nome_alvo)
-
-    def __set__(self, instance, value):
-        if value > 0:
-            setattr(instance, self.nome_alvo, value)
-        else:
-            raise ValueError('valor deve ser > 0')
-
 @entidade
 class ItemPedido(object):
 
